53e4cba31736831b7f4ed7984d015c936bdb0177
Haris Ali Khan <haris989@users.noreply.github.com>
2017-03-18 21:37:26 +0530

    Update NSGA II.py

--- NSGA II.py
+++ NSGA II.py
@@ -1,3 +1,8 @@
+# Program Name: NSGA-II.py
+# Description: This is a python implementation of Prof. Kalyanmoy Deb's popular NSGA-II algorithm
+# Author: Haris Ali Khan 
+# Supervisor: Prof. Manoj Kumar Tiwari
+
 #Importing required modules
 import math
 import random
