af3eedc8f5e62fa646a828e39cabcf31ca0497f0
MazeR <mazer0701@gmail.com>
2018-03-01 21:01:24 +0800

    ptimization_speed

--- NSGA II.py
+++ NSGA II.py
@@ -2,6 +2,7 @@
 # Description: This is a python implementation of Prof. Kalyanmoy Deb's popular NSGA-II algorithm
 # Author: Haris Ali Khan 
 # Supervisor: Prof. Manoj Kumar Tiwari
+# Editor: MazeR <mazer0701@gmail.com>
 
 #Importing required modules
 import math
@@ -10,28 +11,20 @@ import matplotlib.pyplot as plt
 
 #First function to optimize
 def function1(x):
-    value = -x**2
-    return value
+    return -x**2
 
 #Second function to optimize
 def function2(x):
-    value = -(x-2)**2
-    return value
-
-#Function to find index of list
-def index_of(a,list):
-    for i in range(0,len(list)):
-        if list[i] == a:
-            return i
-    return -1
+    return -(x-2)**2
 
 #Function to sort by values
 def sort_by_values(list1, values):
     sorted_list = []
     while(len(sorted_list)!=len(list1)):
-        if index_of(min(values),values) in list1:
-            sorted_list.append(index_of(min(values),values))
-        values[index_of(min(values),values)] = math.inf
+        minValue = values.index(min(values))
+        if minValue in list1:
+            sorted_list.append(minValue)
+        values[minValue] = math.inf
     return sorted_list
 
 #Function to carry out NSGA-II's fast non dominated sort
@@ -86,17 +79,15 @@ def crowding_distance(values1, values2, front):
 
 #Function to carry out the crossover
 def crossover(a,b):
-    r=random.random()
-    if r>0.5:
+    if random.random() > 0.5:
         return mutation((a+b)/2)
     else:
         return mutation((a-b)/2)
 
 #Function to carry out the mutation operator
 def mutation(solution):
-    mutation_prob = random.random()
-    if mutation_prob <1:
-        solution = min_x+(max_x-min_x)*random.random()
+    if random.random() < 1:
+        solution = random.uniform(min_x, max_x)
     return solution
 
 #Main program starts here
@@ -106,19 +97,19 @@ max_gen = 921
 #Initialization
 min_x=-55
 max_x=55
-solution=[min_x+(max_x-min_x)*random.random() for i in range(0,pop_size)]
+solution=[random.uniform(min_x, max_x) for i in range(0, pop_size)]
 gen_no=0
 while(gen_no<max_gen):
     function1_values = [function1(solution[i])for i in range(0,pop_size)]
     function2_values = [function2(solution[i])for i in range(0,pop_size)]
-    non_dominated_sorted_solution = fast_non_dominated_sort(function1_values[:],function2_values[:])
+    non_dominated_sorted_solution = fast_non_dominated_sort(function1_values, function2_values)
     print("The best front for Generation number ",gen_no, " is")
     for valuez in non_dominated_sorted_solution[0]:
         print(round(solution[valuez],3),end=" ")
     print("\n")
     crowding_distance_values=[]
     for i in range(0,len(non_dominated_sorted_solution)):
-        crowding_distance_values.append(crowding_distance(function1_values[:],function2_values[:],non_dominated_sorted_solution[i][:]))
+        crowding_distance_values.append(crowding_distance(function1_values, function2_values, non_dominated_sorted_solution[i]))
     solution2 = solution[:]
     #Generating offsprings
     while(len(solution2)!=2*pop_size):
@@ -127,13 +118,15 @@ while(gen_no<max_gen):
         solution2.append(crossover(solution[a1],solution[b1]))
     function1_values2 = [function1(solution2[i])for i in range(0,2*pop_size)]
     function2_values2 = [function2(solution2[i])for i in range(0,2*pop_size)]
-    non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2[:],function2_values2[:])
+    non_dominated_sorted_solution2 = fast_non_dominated_sort(function1_values2, function2_values2)
     crowding_distance_values2=[]
     for i in range(0,len(non_dominated_sorted_solution2)):
-        crowding_distance_values2.append(crowding_distance(function1_values2[:],function2_values2[:],non_dominated_sorted_solution2[i][:]))
+        crowding_distance_values2.append(crowding_distance(function1_values2, function2_values2, non_dominated_sorted_solution2[i]))
     new_solution= []
     for i in range(0,len(non_dominated_sorted_solution2)):
-        non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j],non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
+        non_dominated_sorted_solution2_1 = [non_dominated_sorted_solution2[i].index(
+                non_dominated_sorted_solution2[i][j]
+            ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
         front22 = sort_by_values(non_dominated_sorted_solution2_1[:], crowding_distance_values2[i][:])
         front = [non_dominated_sorted_solution2[i][front22[j]] for j in range(0,len(non_dominated_sorted_solution2[i]))]
         front.reverse()
